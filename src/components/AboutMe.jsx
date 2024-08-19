1a29af009bcf1b0a0faa5d97805b8032c4998d3d
Gustavo Müller Leonini Machado <gustavomuller160305@gmail.com>
2024-08-19 15:40:16 -0300

    icons in AboutMe and Education

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,7 +1,10 @@
 function AboutMe() {
     return (
         <section>
-            <h1 className="text-[#516264] text-xl">About Me</h1>
+            <div className="flex items-center">
+                <img className="w-5 h-5 mr-3" src="/user.png"></img>
+                <h1 className="text-[#516264] text-xl">About Me</h1>
+            </div>
             <p className="text-[#c7dbde]">
                 Hello, my name is Gustavo Müller, and I am a Software Developer. I have extensive academic activities, both individual and group-based. I am very dedicated to studying the field and completing challenges, whether they are personal or work-related. My experience includes developing websites, configuring, and testing applications in my current role at GBX Brasil. I value good communication, strong relationships with colleagues, total dedication to achieving my goals, and a lifelong commitment to studying technology.
             </p>
--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,10 +1,17 @@
 function Education() {
     return (
         <section id="education">
-            <h1 className="text-[#516264] text-xl">Education</h1>
+            <div className="flex items-center">
+                <img className="w-6 h-7 mr-3" src="/chapeu.png"></img>
+                <h1 className="text-[#516264] text-xl">Education</h1>
+            </div>
+            
             <p className="text-[#c7dbde]">
                 Graduated from high school at Senac Distrido Criativo, where I was introduced to and developed my passion for technology through the technical programming course integrated with high school education. I am currently pursuing a degree in Systems Analysis and Development at Centro Universitário Senac. I participated in the Geração Caldeira 2024 project at Instituto Caldeira, where I had access to Alura’s platform courses and earned 10 certificates of completion in various courses.
             </p>
+            <div>
+                
+            </div>
         </section>
     )
 }
