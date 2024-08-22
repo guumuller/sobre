5a525e65769707c6dffcfa8148410dc953fa03e9
Gustavo Müller Leonini Machado <gustavomuller160305@gmail.com>
2024-08-22 15:52:48 -0300

    link email

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
     return (
         <footer className="text-center flex justify-between">
-          <a className="text-cyan-600 hover:text-cyan-500 cursor-pointer" target="_blank">✉️ Contact Me →</a>
+          <a className="text-cyan-600 hover:text-cyan-500 cursor-pointer" target="_blank" href="https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox?compose=CllgCJqZhZQtmzslKmDNRNWZccNWdclNmPQjwvdRfTTpHVNMqvqKxnzDVlxnCrwFXCSJZKSkjZg">✉️ Contact Me →</a>
           <a className="text-cyan-600 hover:text-cyan-500 cursor-pointer" href="#" download="#">Download Curriculum ↓</a>
         </footer>
     )
